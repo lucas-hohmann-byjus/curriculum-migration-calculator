8baec0210b5d52472c1da97678ce559c318d5cce
Lucas Hohmann <89453345+lucas-hohmann-byjus@users.noreply.github.com>
2023-10-17 16:20:47 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+from consts import *
+
+st.set_page_config(
+    page_title="Curriculum Migration",
+    page_icon="⏩",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Dados do Aluno")
+curriculum = st.selectbox("Currículo", ["BEG", "INT", "ADV", "PRO"])
+direction = st.selectbox("Direção", ["1:M para 1:1"])
+class_number = st.number_input("Última aula concluída", min_value=1, max_value=144)
+
+
+# Find destination
+for src, dst in TABLE[curriculum]:
+    if src == class_number:
+        break
+else:
+    dst = None
+
+if dst:
+    st.header(f"O aluno pode ser migrado para a aula '{curriculum} C{dst}'")
+else:
+    st.title("AULA INVÁLIDA")
