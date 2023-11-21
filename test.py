f3a858963afc7b68521c40c9f9b58155eb443925
Lucas Hohmann <lfhohmann@gmail.com>
2023-11-21 14:47:58 -0300

    Test 2

---
+++ test.py
@@ -0,0 +1,12 @@
+CURRICULUMS = {
+    "BEG": {
+        "1:1": {
+            1: "SpriteLab",
+            30: "AppLab 1",
+            57: "GameLab",
+            79: "AppLab 2",
+            107: "Teachable Machine",
+            113: "PictoBlox",
+        }
+    },
+}
